06a2dd62815fd247557e8307db0cd8ebfe4e5856
LIkeduh124 <spencer.farley@stu.daviess.kyschools.us>
2026-02-02 18:10:18 +0000

    bleh leh lhe this stinky

--- MusicDownloads.java
+++ MusicDownloads.java
@@ -7,14 +7,30 @@ public class MusicDownloads{
   public DownloadInfo getDownloadInfo(String title)
   {
     /* To be implemented in part (a) */
-
+    for(int a = 0; a<downloadList.size(); a++)
+    {
+      if(downloadList.getTitle(a).equals(title))
+      {
+        return downloadInfo(downloadList(a));
+      }
+    }
+    return null;
 
   }
 
   public void updateDownloadInfo(List<String> titles)
   {
     /* To be implemented in part (b) */
-
+    for(int a = 0; a<titles.size(); a++)
+    {
+      for(int b = 0; b<downloadList.size(); b++)
+      {
+        if(titles.getTitle(a).equals(downloadList.getTitle(b)))
+        {
+          downloadList.incrementTimesDownLoaded();
+        }
+      }
+    }
 
 
   }
